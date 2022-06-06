725df04db0135d67738924a03b0a8ba830108c51
Harin <57610449+gharynsl123@users.noreply.github.com>
2022-06-06 14:54:58 +0700

    gift retrofit

--- app/src/main/java/com/example/finalproject/data/network/quran/QuranApiService.kt
+++ app/src/main/java/com/example/finalproject/data/network/quran/QuranApiService.kt
@@ -3,6 +3,7 @@ package com.example.finalproject.data.network.quran
 import com.example.finalproject.data.response.quranres.QuranResponse
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface QuranApiService {
 
