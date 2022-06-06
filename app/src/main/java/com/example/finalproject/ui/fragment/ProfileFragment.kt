bfee3fb5fbc2d295a9cc2745b76a327832012d37
Harin <57610449+gharynsl123@users.noreply.github.com>
2022-06-06 13:33:34 +0700

    cleaning my code

--- app/src/main/java/com/example/finalproject/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/fragment/ProfileFragment.kt
@@ -2,8 +2,10 @@ package com.example.finalproject.ui.fragment
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +18,7 @@ import androidx.fragment.app.Fragment
 import com.example.finalproject.R
 import com.example.finalproject.databinding.FragmentProfileBinding
 import com.example.finalproject.ui.LoginActivity
+import com.example.finalproject.ui.main.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
@@ -24,9 +27,6 @@ class ProfileFragment : Fragment() {
     private val binding get() = _binding as FragmentProfileBinding
 
     private lateinit var fireBaseAuth: FirebaseAuth
-
-    private val CHANNEL_ID = "channel_id_example_01"
-    private val notificationId = 101
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,49 +42,11 @@ class ProfileFragment : Fragment() {
             chekUser()
         }
 
-        createNotificationChannel()
-
-        binding.btnNotification.setOnClickListener {
-            sendNotification()
-        }
-
         return binding.root
 
 
     }
 
-    private fun createNotificationChannel() {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val title = "Notification Title"
-            val descriptionText = "Notification Description"
-            val important = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, title, important).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    private fun sendNotification() {
-        val builder = activity?.let {
-            NotificationCompat.Builder(it.applicationContext, CHANNEL_ID).setSmallIcon(
-                R.drawable.icon_app
-            )
-                .setContentTitle("Kareem App")
-                .setContentText("Akhi Jangan Lupa Dzikir Pagi")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        }
-        with(
-            activity?.applicationContext?.let {
-                NotificationManagerCompat.from(it)
-            }
-        ) {
-            builder?.let { this?.notify(notificationId, it.build()) }
-        }
-    }
 
     private fun chekUser() {
         val firebaseUser = fireBaseAuth.currentUser
